5e17f45c8ee3c5abf38bb8378ad29f58c23ad845
Gláucio Dias <fleevrl@gmail.com>
2023-03-22 23:51:21 -0300

    add ConvidadosController

---
+++ src/main/java/com/demo/festa/controller/ConvidadosController.java
@@ -0,0 +1,13 @@
+package com.demo.festa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConvidadosController {
+	
+	@GetMapping("/convidados")
+	public String listar() {
+		return "ListaConvidados";
+	}
+}
