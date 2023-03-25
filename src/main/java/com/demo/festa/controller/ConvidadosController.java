54436bcdd1db3b8e48199e8a150023d04ff44209
Gláucio Dias <fleevrl@gmail.com>
2023-03-24 23:27:04 -0300

    Update ConvidadosController.java

--- src/main/java/com/demo/festa/controller/ConvidadosController.java
+++ src/main/java/com/demo/festa/controller/ConvidadosController.java
@@ -1,13 +1,25 @@
 package com.demo.festa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.demo.festa.repository.Convidados;
 
 @Controller
 public class ConvidadosController {
-	
+
+	@Autowired
+	private Convidados convidados;
+
 	@GetMapping("/convidados")
-	public String listar() {
-		return "ListaConvidados";
+	public ModelAndView listar() {
+
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+
+		modelAndView.addObject("convidados", convidados.findAll());
+
+		return modelAndView;
 	}
 }
