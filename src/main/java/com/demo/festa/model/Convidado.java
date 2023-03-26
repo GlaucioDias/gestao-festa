6b5842901b092443dd9479969f412ae501180871
Gláucio Dias <fleevrl@gmail.com>
2023-03-26 18:25:17 -0300

    Update Convidado.java

--- src/main/java/com/demo/festa/model/Convidado.java
+++ src/main/java/com/demo/festa/model/Convidado.java
@@ -1,23 +1,28 @@
 package com.demo.festa.model;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Convidado implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
+public class Convidado {
 	
 	@Id
-	@GeneratedValue
-	private Long id;
-	
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+
 	private String nome;
 	
 	private Integer quantidadeAcompanhantes;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
