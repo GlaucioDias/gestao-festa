cf065d22eedb3df6e6b372d7f045414acacb0237
Gláucio Dias <fleevrl@gmail.com>
2023-03-25 22:02:07 -0300

    Update ConvidadosController.java
    - Add RequestMapping
    - Create PostMapping

--- src/main/java/com/demo/festa/controller/ConvidadosController.java
+++ src/main/java/com/demo/festa/controller/ConvidadosController.java
@@ -3,23 +3,35 @@ package com.demo.festa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.demo.festa.model.Convidado;
 import com.demo.festa.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
 	@Autowired
 	private Convidados convidados;
 
-	@GetMapping("/convidados")
+	@GetMapping()
 	public ModelAndView listar() {
 
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 
 		modelAndView.addObject("convidados", convidados.findAll());
+		
+		modelAndView.addObject(new Convidado());
 
 		return modelAndView;
+	}
+	
+	@PostMapping()
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
 	}
 }
