16a48159a2dfd4930ad89f8e4dbdb2be21485e8c
Gláucio Dias <fleevrl@gmail.com>
2023-03-26 18:29:28 -0300

    Update ConvidadosController.java

--- src/main/java/com/demo/festa/controller/ConvidadosController.java
+++ src/main/java/com/demo/festa/controller/ConvidadosController.java
@@ -2,36 +2,33 @@ package com.demo.festa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.demo.festa.model.Convidado;
 import com.demo.festa.repository.Convidados;
 
 @Controller
-@RequestMapping("/convidados")
+@RequestMapping("/")
 public class ConvidadosController {
 
 	@Autowired
 	private Convidados convidados;
 
-	@GetMapping()
+	@RequestMapping(value = { "/", "/convidados" }, method = RequestMethod.GET)
 	public ModelAndView listar() {
-
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
-
-		modelAndView.addObject("convidados", convidados.findAll());
-		
+		modelAndView.addObject("convidados", convidados.findAll());		
 		modelAndView.addObject(new Convidado());
-
 		return modelAndView;
 	}
 	
-	@PostMapping()
-	public String salvar(Convidado convidado) {
+	@RequestMapping(value = { "/", "/convidados" }, method = RequestMethod.POST)
+	public String salvar(@ModelAttribute Convidado convidado){
 		this.convidados.save(convidado);
-		return "redirect:/convidados";
+		return "redirect:/";
 	}
+
 }
