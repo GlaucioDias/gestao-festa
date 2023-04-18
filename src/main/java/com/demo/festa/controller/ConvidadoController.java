545f4e6973dbf6f4c70bd19275fc1c422dca8c0f
Gláucio Dias <fleevrl@gmail.com>
2023-04-18 10:51:28 -0300

    Update ConvidadoController.java

--- src/main/java/com/demo/festa/controller/ConvidadoController.java
+++ src/main/java/com/demo/festa/controller/ConvidadoController.java
@@ -1,25 +1,34 @@
 package com.demo.festa.controller;
 
 import java.util.List;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.demo.festa.model.Convidado;
 import com.demo.festa.repository.Convidados;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping(value={"/"})
 public class ConvidadoController {
 
 	@Autowired
 	private Convidados convidados;
+	
 
-	@RequestMapping(value = { "/", "/convidados" }, method = RequestMethod.GET)
+    @RequestMapping("/")
+    public String root(Locale locale) {
+        return "redirect:/convidados";
+    }
+
+	@RequestMapping(value={"/convidados"}, method = RequestMethod.GET)
 	public ModelAndView listar() {
 		List<Convidado> listaConvidados = this.convidados.findAll();
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
@@ -28,16 +37,18 @@ public class ConvidadoController {
 		return modelAndView;
 	}
 	
-	@RequestMapping(value = { "/", "/convidados" }, method = RequestMethod.POST)
-	public String salvar(@ModelAttribute Convidado convidado){
+	@RequestMapping(value={"/convidados"}, method = RequestMethod.POST)
+	public String salvar(@ModelAttribute Convidado convidado, RedirectAttributes redirectAttr){
 		this.convidados.save(convidado);
-		return "redirect:/";
+		redirectAttr.addFlashAttribute("message", "Convidado adicionado!");
+		return "redirect:/convidados";
 	}
 	
-	@RequestMapping(value="/convidado/excluir", method = RequestMethod.POST)
-	public String removeConvidado(@RequestParam("id") long id) {
+	@RequestMapping(value="/convidados/{id}/remover", method = RequestMethod.POST)
+	public String removeConvidado(@PathVariable Long id, RedirectAttributes redirectAttr) {
 		this.convidados.deleteById(id);
-		return "redirect:/";
+		redirectAttr.addFlashAttribute("message", "Convidado removido!");
+		return "redirect:/convidados";
 	}
 	
 
